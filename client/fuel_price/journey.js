a0a74eef48839bc5aacfca6846fe902c329f9cda
aaron <aaron.low.au@gmail.com>
2015-09-28 22:24:13 +0800

    restructuring client side application

---
+++ client/fuel_price/journey.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function Journey(line, prices) {
+    // geo json line for journey
+    this.line = line;
+    // array of prices
+    this.prices = prices;
+    if (this.prices.length) {
+        this.prices[0].isLowest = true;
+    }
+}
+
+module.exports = Journey;
